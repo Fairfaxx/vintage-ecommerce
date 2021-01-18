bee31b31d82573cfbe689f81670dd2667b2f3895
Fede Lemaire <fedelemaire@Fedes-MacBook-Pro-2.local>
2021-01-18 20:01:44 -0300

    cambios de componentes

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,18 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import Item from '../Item/Item';
 import style from './ItemCount.module.scss';
 
 
-
-
-
-const ItemCount = () => {
-    const initialValue = [
-        { id: 1, nombre: 'Radio Antigua', precio: 9000 },
-        { id: 2, nombre: 'Reloj Antiguo', precio: 19000 },
-        { id: 3, nombre: 'Pulsera Antigua', precio: 15000 },
-    ]
-    const [items, setItems] = useState([])
+const ItemCount = () => {   
     const [count, setCount] = useState(0)
 
     const handleDescount = () => {
@@ -22,36 +13,23 @@ const ItemCount = () => {
         setCount(count - 1)
     }
     const handleCount = () => {
-        if (count >= items.length) {
-            return
-        }
+        // if (count >= items.length) {
+        //     return
+        // }
         setCount(count + 1)
     }
-
-    useEffect(() => {
-        const callData = new Promise((res, rej) => {
-            setTimeout(() => res(initialValue), 3000);
-        })
-
-        callData.then((res) => {
-            setItems(res)
-        })
-
-        // callData()
-
-    }, [])
 
     return (
         <div className={style.container}>
             <h3 className={style.item_title}>Items</h3>
-            {
+            {/* {
                 items.map(item => (
                     <Item
                         key={item.id}
                         item={item}
                     />
                 ))
-            }
+            } */}
             <p className={style.counter}><button className={style.btn_product} onClick={() => handleDescount()}>-</button>{count}<button className={style.btn_product} onClick={() => handleCount()}>+</button></p>
         </div>
     )
---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import Item from '../Item/Item';
+const ItemList = () => {
+
+    const initialValue = [
+        { id: 1, nombre: 'Radio Antigua', precio: 9000 },
+        { id: 2, nombre: 'Reloj Antiguo', precio: 19000 },
+        { id: 3, nombre: 'Pulsera Antigua', precio: 15000 },
+    ]
+    const [items, setItems] = useState([])
+    
+    useEffect(() => {
+        const callData = new Promise((res, rej) => {
+            setTimeout(() => res(initialValue), 500);
+        })
+
+        callData.then((res) => {
+            setItems(res)
+        })
+        
+        // callData()
+
+    }, [])
+
+    return (
+        <div>
+            {
+                items.map(item => (
+                    <Item
+                        key={item.id}
+                        item={item}
+                    />
+                ))
+            }
+        </div>
+    )
+}
+
+export default ItemList
