af55903ab4b1010e210fcbe0169173da0abba66c
Fede <53632624+Fairfaxx@users.noreply.github.com>
2021-01-20 23:03:52 -0300

    Update ItemDetail.js

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const ItemDetail = () => {
+
+import ItemCount from '../ItemCount/ItemCount';
+
+const ItemDetail = ({ newItemDetail }) => {
+
+    const { id, nombre, precio } = newItemDetail;
     return (
         <div>
-
+            <h2 style={{ backgroundColor: 'red' }}>{nombre}</h2>
+            <h3>Valor: ${precio}</h3>
+            <ItemCount />
         </div>
-    )
-}
+    );
+};
 
 export default ItemDetail;
