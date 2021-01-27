158926e04e050a865e46f7b80a73ebf1bf3c896f
Fede Lemaire <fede.lemaire@gmail.com>
2021-01-26 23:00:43 -0300

    cambios en componentes

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ import CartWidget from './components/CartWidget/CartWidget';
 
 function App(props) {
 
-  // const initialValue = [
-  //   { id: 1, nombre: 'Radio Antigua', precio: 9000, quantity: 5, categoryId: 'radios' },
-  //   { id: 2, nombre: 'Reloj Antiguo', precio: 19000, quantity: 5, categoryId: 'relojes' },
-  //   { id: 3, nombre: 'Pulsera Antigua', precio: 15000, quantity: 5, categoryId: 'pulseras' },
-  // ]
+  const initialValue = [
+    { id: 1, nombre: 'Radio Antigua', precio: 9000, quantity: 5, categoryId: 'radios' },
+    { id: 2, nombre: 'Reloj Antiguo', precio: 19000, quantity: 5, categoryId: 'relojes' },
+    { id: 3, nombre: 'Pulsera Antigua', precio: 15000, quantity: 5, categoryId: 'pulseras' },
+  ]
 
   return (
     <div className="App">
@@ -26,13 +26,13 @@ function App(props) {
         </Navbar>
         <Switch>
           <Route exact path="/" >
-            <ItemListContainer titulo="Bienvenidos a Vintage E-commerce" />
+            <ItemListContainer initialValue={initialValue} titulo="Bienvenidos a Vintage E-commerce" />
           </Route>
-          <Route path="/category/:id">
+          <Route path="/category/:categoryId">
             <ItemListContainer titulo="Bienvenidos a Vintage E-commerce" />
           </Route>
           <Route path="/item/:id">
-            <ItemDetailContainer />
+            <ItemDetailContainer initialValue={initialValue} />
           </Route>
           {/* <Route exact path="/item/:1">
             <ItemDetail />
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import styles from './ItemListContainer.module.scss';
+import Item from '../Item/Item'
 
-const ItemListContainer = ({ titulo }) => {
+const ItemListContainer = ({ titulo, initialValue }) => {
+
+    const { categoryId } = useParams();
+
+    const [newItemDetail, setNewItemDetail] = useState([]);
+    // const [onAdd, setOnAdd] = useState(false)
+
+    useEffect(() => {
+        const emulateFetch = () => {
+            let findItems = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    initialValue ? resolve(initialValue) : reject('No items available');
+                }, 3000);
+            });
+
+            findItems
+                .then((resolve) => {
+                    console.log('ITEMS FOUNDED! ', resolve);
+                    setNewItemDetail(resolve);
+                })
+                .catch((err) => {
+                    console.log('Error: ', err);
+                });
+        };
+        emulateFetch()
+
+    }, [newItemDetail]);
+
     return (
         <div className={styles.container}>
-            <h1 className={styles.main_title}>{titulo}</h1>
+            {newItemDetail &&
+                newItemDetail.map(item => (
+                    <Item
+                        key={item.id}
+                        item={item}
+                    />
+                ))
+            }
         </div>
     )
 }
