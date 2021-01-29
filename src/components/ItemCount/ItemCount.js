2d66051f884f0de978ddb09bb87a6f781fb1baf6
Fede Lemaire <fede.lemaire@gmail.com>
2021-01-29 17:13:16 -0300

    fontawesome iconos en + y - del counter

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -13,9 +13,9 @@ const ItemCount = ({ currentItem, value, stock, handleCount, handleDescount, onA
             {onAdding ?
                 <>
                     <p className={style.counter}>
-                        <button className={style.btn_product} onClick={() => handleDescount()}>-</button>
+                        <button className={style.btn_product} onClick={() => handleDescount()}><i class="fas fa-minus"></i></button>
                         {count}
-                        <button className={style.btn_product} onClick={() => handleCount()}>+</button>
+                        <button className={style.btn_product} onClick={() => handleCount()}><i class="fas fa-plus"></i></button>
                     </p>
                     <Link to="/cart"><button className={style.btn_product}>Terminar mi compra {count}</button></Link>
                 </>
